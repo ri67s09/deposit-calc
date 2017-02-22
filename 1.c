d2c4c2d05de5d31a16b4683a1a778f313ee3dfb9
Миллер Яна Валерьевна <ri67s09@mwp07.csc.local>
2017-02-22 09:11:50 +0700

    1

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+	int days, sum, p, d;
+	printf("Введите сумму вклада от 10 т.р.: ");
+	scanf("%d", &sum);
+	printf("Введите срок вклада не более 365 дней: ");
+	scanf("%d", &days);
+	
+}
